dfc2f506a21ffc5ebd6234452df182ce5a1164c2
Sevser <44920040+Sevser@users.noreply.github.com>
2022-10-16 17:56:44 +0300

    Time: 159 ms (13.56%), Space: 50 MB (86.05%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currentSubarray = nums[0];
+    let maxSubarray = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        currentSubarray = Math.max(nums[i], currentSubarray + nums[i]);
+        maxSubarray = Math.max(maxSubarray, currentSubarray);
+    }
+    return maxSubarray;
+};
