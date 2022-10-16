4d4bf4b3b102ea489a799660e3a7ebbb661202b1
Sevser <44920040+Sevser@users.noreply.github.com>
2022-10-16 17:49:11 +0300

    Time: 128 ms (5.76%), Space: 42.5 MB (33.90%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let count = 0;
+    let max = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') count++;
+        if (s[i] === ')') count--;
+        if (count > max)
+            max = count;
+    }
+    return max;
+};
