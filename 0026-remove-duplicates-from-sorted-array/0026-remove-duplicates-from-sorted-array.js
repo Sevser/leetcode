7a2377261af95f821ccf99839718422e0dd6c217
Sevser <44920040+Sevser@users.noreply.github.com>
2022-10-16 17:54:50 +0300

    Time: 148 ms (32.02%), Space: 46 MB (8.13%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    nums.splice(0, nums.length, ...(new Set(nums.values())).keys());
+    return nums.length;
+};
